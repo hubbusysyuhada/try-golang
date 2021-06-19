15032181f7320c05b807eaf3115031e032df0703
hubbusysyuhada <hubbusysyuhada@gmail.com>
2021-06-19 15:56:04 +0700

    try basic api

--- app.go
+++ app.go
@@ -1,8 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 func main() {
-	greeting := "hello world"
-	fmt.Println(greeting)
+	fmt.Println("masuk main")
+	router := mux.NewRouter()
+	router.HandleFunc("/", Home).Methods("GET")
+	log.Fatal(http.ListenAndServe(":8080", router))
+}
+
+func Home(writer http.ResponseWriter, request *http.Request) {
+	writer.WriteHeader(200)
+	json.NewEncoder(writer).Encode(map[string]interface{}{
+		"status":  200,
+		"message": "hello world",
+	})
+	fmt.Println("ini home")
 }
