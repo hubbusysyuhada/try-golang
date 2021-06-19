5748c586588c267c9518cc6d6e8855c77fc36446
hubbusysyuhada <hubbusysyuhada@gmail.com>
2021-06-19 22:47:42 +0700

    slice array

--- app.go
+++ app.go
@@ -3,6 +3,20 @@ package main
 import "fmt"
 
 func main() {
-	greeting := "hello world"
-	fmt.Println(greeting)
+	countries := [...]string{
+		"indonesia",
+		"singapura",
+		"amerika",
+		"afrika",
+	}
+
+	sliced_array := countries[0:]
+	new_sliced := append(sliced_array, "jerman")
+	new_sliced = append(new_sliced, "venezuela")
+	fmt.Println(new_sliced)
+
+	new_array := make([]string, 2, 10)
+	new_array[0] = "bola"
+	new_array[1] = "kaos"
+	fmt.Println(new_array, "<<< new array")
 }
